cca8ada115c7a44aac312d0a561d258a7c1b6285
rdwanmuhamad <mrdwn624@gmail.com>
2023-07-07 12:06:33 +0800

    penggunaan object.create()

--- object (revisited)/script.js
+++ object (revisited)/script.js
@@ -1,6 +1,7 @@
 // cara untuk membuat object pada javascript
 
 // object literal
+// problem : efektif untuk object yang banyak
 // let hero1 = {
 //   name: "Muhamad Ridwan",
 //   health: 10,
@@ -20,42 +21,52 @@
 // };
 
 // function declaration
-// function Hero(name, health) {
-//   let hero = {};
-//   hero.name = name;
-//   hero.health = health;
+// problem :
+const methodHero = {
+  energy: function (eat) {
+    this.health += eat;
+    console.log(`Congratulations ${this.name}, your energy increases!`);
+  },
 
-//   hero.energy = function (eat) {
+  activity: function (jump) {
+    this.health -= jump;
+    console.log(`Sorry ${this.name}, sorry your energy is low!`);
+  },
+
+  sleep: function (time) {
+    this.health += time * 2;
+    console.log(`Congratulations ${this.name}, your energy increases!`);
+  },
+};
+
+function Hero(name, health) {
+  let hero = Object.create(methodHero);
+  hero.name = name;
+  hero.health = health;
+//   hero.energy = methodHero.energy;
+//   hero.activity = methodHero.activity;
+//   hero.sleep = methodHero.sleep;
+  return hero;
+}
+
+let ridwan = Hero("Ridwan", 10);
+let muhamad = Hero("Muhamad", 10);
+
+// constructor function
+// function Hero(name, health) {
+//   this.name = name;
+//   this.health = health;
+
+//   this.energy = function (eat) {
 //     this.energy += eat;
 //     console.log(`Congratulations ${this.name}, your energy increases!`);
 //   };
 
-//   hero.activity = function(jump){
-//     this.energy -= jump
+//   this.activity = function (jump) {
+//     this.energy -= jump;
 //     console.log(`Sorry ${this.name}, sorry your energy is low!`);
-//   }
-
-//   return hero;
+//   };
 // }
 
-// let ridwan = Hero("Ridwan", 10);
-// let muhamad = Hero("Muhamad", 10);
-
-// constructor function
-function Hero(name, health) {
-  this.name = name;
-  this.health = health;
-
-  this.energy = function (eat) {
-    this.energy += eat;
-    console.log(`Congratulations ${this.name}, your energy increases!`);
-  };
-
-  this.activity = function (jump) {
-    this.energy -= jump;
-    console.log(`Sorry ${this.name}, sorry your energy is low!`);
-  };
-}
-
-let ridwan = new Hero("Ridwan", 10);
-let muhamad = new Hero("Muhamad", 10);
+// let ridwan = new Hero("Ridwan", 10);
+// let muhamad = new Hero("Muhamad", 10);
