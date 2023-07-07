2620cd1363e349055c7dee99438ffc3d0eb980ad
rdwanmuhamad <mrdwn624@gmail.com>
2023-07-07 12:38:01 +0800

    implementasi prototype

--- object (revisited)/script.js
+++ object (revisited)/script.js
@@ -6,7 +6,7 @@
 //   name: "Muhamad Ridwan",
 //   health: 10,
 //   energy: function (eat) {
-//     this.energy = this.energy + eat;
+//     this.health = this.health + eat;
 //     console.log(`Congratulations ${this.name}, energy increases!`);
 //   },
 // };
@@ -15,41 +15,41 @@
 //   name: "Ridwan Muhamad",
 //   health: 30,
 //   energy: function (eat) {
-//     this.energy = this.energy + eat;
+//     this.health = this.health + eat;
 //     console.log(`Congratulations ${this.name}, energy increases!`);
 //   },
 // };
 
 // function declaration
-const methodHero = {
-  energy: function (eat) {
-    this.health += eat;
-    console.log(`Congratulations ${this.name}, your energy increases!`);
-  },
+// const methodHero = {
+//   energy: function (eat) {
+//     this.health += eat;
+//     console.log(`Congratulations ${this.name}, your energy increases!`);
+//   },
 
-  activity: function (jump) {
-    this.health -= jump;
-    console.log(`Sorry ${this.name}, sorry your energy is low!`);
-  },
+//   activity: function (jump) {
+//     this.health -= jump;
+//     console.log(`Sorry ${this.name}, sorry your energy is low!`);
+//   },
 
-  sleep: function (time) {
-    this.health += time * 2;
-    console.log(`Congratulations ${this.name}, your energy increases!`);
-  },
-};
+//   sleep: function (time) {
+//     this.health += time * 2;
+//     console.log(`Congratulations ${this.name}, your energy increases!`);
+//   },
+// };
 
-function Hero(name, health) {
-  let hero = Object.create(methodHero);
-  hero.name = name;
-  hero.health = health;
-//   hero.energy = methodHero.energy;
-//   hero.activity = methodHero.activity;
-//   hero.sleep = methodHero.sleep;
-  return hero;
-}
+// function Hero(name, health) {
+//   let hero = Object.create(methodHero);
+//   hero.name = name;
+//   hero.health = health;
+// //   hero.energy = methodHero.energy;
+// //   hero.activity = methodHero.activity;
+// //   hero.sleep = methodHero.sleep;
+//   return hero;
+// }
 
-let ridwan = Hero("Ridwan", 10);
-let muhamad = Hero("Muhamad", 10);
+// let ridwan = Hero("Ridwan", 10);
+// let muhamad = Hero("Muhamad", 10);
 
 // constructor function
 // function Hero(name, health) {
@@ -57,15 +57,62 @@ let muhamad = Hero("Muhamad", 10);
 //   this.health = health;
 
 //   this.energy = function (eat) {
-//     this.energy += eat;
+//     this.health += eat;
 //     console.log(`Congratulations ${this.name}, your energy increases!`);
 //   };
 
 //   this.activity = function (jump) {
-//     this.energy -= jump;
+//     this.health -= jump;
 //     console.log(`Sorry ${this.name}, sorry your energy is low!`);
 //   };
 // }
 
 // let ridwan = new Hero("Ridwan", 10);
 // let muhamad = new Hero("Muhamad", 10);
+
+// prototype
+function Hero(name, health) {
+  this.name = name;
+  this.health = health;
+}
+
+Hero.prototype.energy = function (eat) {
+  this.health += eat;
+  return `Congratulations ${this.name}, your energy increases!`;
+};
+
+Hero.prototype.activity = function (jump) {
+  this.health -= jump;
+  return `Sorry ${this.name}, sorry your energy is low!`;
+};
+
+Hero.prototype.sleep = function (time) {
+  this.health += time * 2;
+  return `Congratulations ${this.name}, your energy increases!`;
+};
+
+let ridwan = new Hero("Ridwan", 10);
+
+// versi menggunakan class
+// class Hero {
+//   constructor(name, health) {
+//     this.name = name;
+//     this.health = health;
+//   }
+//   energy(eat) {
+//     this.health += eat;
+//     return `Congratulations ${this.name}, your energy increases!`;
+//   }
+
+//   activity(jump) {
+//     this.health -= jump;
+//     return `Sorry ${this.name}, sorry your energy is low!`;
+//   }
+
+//   sleep(time) {
+//     this.health += time * 2;
+//     return `Congratulations ${this.name}, your energy increases!`;
+//   }
+// }
+
+// let ridwan = new Hero("Ridwan", 10);
