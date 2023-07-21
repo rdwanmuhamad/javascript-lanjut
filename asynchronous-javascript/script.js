40be43820a7bfe43f60005c9e28ca481b714aef0
rdwanmuhamad <mrdwn624@gmail.com>
2023-07-21 12:08:49 +0800

    asynchoronous javascript

---
+++ asynchronous-javascript/script.js
@@ -0,0 +1,15 @@
+// referensi http://latentflip.com/loupe/
+
+$.ajax("button", "click", function onClick() {
+  setTimeout(function timer() {
+    console.log("You clicked the button!");
+  }, 2000);
+});
+
+console.log("Hi!");
+
+setTimeout(function timeout() {
+  console.log("Click the button!");
+}, 5000);
+
+console.log("Welcome to loupe.");
